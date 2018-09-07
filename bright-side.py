ec45e351779928925c8ed0447ddd15bcf31dbb60
Thomas <thomas@mcquitty.net>
2018-09-07 10:10:16 -0500

    Adding Changes

--- bright-side.py
+++ bright-side.py
@@ -2,6 +2,7 @@ from brightOps import brightOps
 import argparse
 import sys
 from subprocess import call
+import traceback
 
 def cleanup():
     call("del", "end*.txt")
@@ -17,14 +18,33 @@ def main(args):
     #parser.add_argument("-p", "--package", help="Package Name", type=str)
     parser.add_argument("-d", "--debug",  help="Debugging Information", type=int)
     parser.add_argument("-a", "--all", help="Gets all files based upon Env, Sys and Sub search", dest='all', default=False, action='store_true')
+    parser.add_argument("-l", "--componentlist", help="Gets component and input components", default=False, action="store_true")
+    parser.add_argument("-comp", "--component", help="Component Name to Download", type=str)
+    parser.add_argument("-e", "--element", help="Element to Download", type=str)
+    parser.add_argument("-et", "--elementType", help="Element Type for Download", type=str)
+    parser.add_argument("-stg", "--stageNumber", help="Stage Number for Element Download", type=str)
+
     args = parser.parse_args()
+    #        num, member, vvll, date, time, system, subsystem, element, elementType, stg, ste, environment = element
 
 
 
     bright = brightOps(args.Instance, args.Environment, args.System, args.Subsystem)
 
-    
-    if(args.all):
+    if(args.componentlist):
+        element = ["", args.element, "", "", args.Environment, args.System, args.Subsystem, args.element, args.elementType, args.stageNumber, "", args.Environment]
+        print element
+        elements = bright.getInputComponentList(element)
+        elementList = bright.getElementList(elements)
+        for components in elementList:
+            print "Component Print"
+            print components
+            num, member, vvll, date, time, system, subsystem, element, element_type, stg, ste, environment = components
+            print member, system, subsystem, element, element_type, stg, environment
+            bright = brightOps("", "", "", "")
+            bright.get_files_by_details(components)
+            bright.getInputComponentList(components)        
+    elif(args.all):
         print "Getting all files"
         bright.get_all_files()
     else:
@@ -40,6 +60,8 @@ if __name__ == '__main__':
         main(sys.argv[1:])
     except Exception, details:
         print "Details of error %s" % details
+        print Exception, details
+        traceback.print_exc()
         sys.exit(1)
 
     sys.exit(0)
--- brightOps.py
+++ brightOps.py
@@ -10,6 +10,56 @@ class brightOps:
         self.environment = environment
         self.system = system
         self.subsystem = subsystem
+
+    def getInputComponentList(self, element):
+        num, member, vvll, date, time, system, subsystem, element, elementType, stg, ste, environment = element
+        cmdLine = "bright.cmd endevor print comp MCQTH01 --env {} --sys {} --sub {} --typ {} -i WEBSALC --sn {}".format(environment, system, subsystem, elementType, stg)
+        output = subprocess.check_output(cmdLine.split())
+        lines = output.split("\n")
+        successCheck = "finished with 0000"
+        failureCheck = "finished with 0004 *NOTFND*"
+        for i in reversed(lines):
+                if successCheck in i:
+                    print "Success!"
+                    return lines
+        for i in reversed(lines):
+                if failureCheck in i:
+                    print "found failure"
+                    return
+        print lines
+        return lines
+
+
+    def getElementList(self,infile):
+        elementList = list()
+        copy = False
+        for line in infile:
+                if "INPUT COMPONENTS" in line:
+                    copy = True
+                elif "OUTPUT COMPONENTS" in line:
+                    copy = False
+                elif copy:
+                    if "+" in line:
+                        print line
+                        temp = line.split()
+                        if(len(temp) > 3):
+                            elementList.append(temp)
+        return elementList
+
+
+    def get_files_by_details(self, details):
+        num, member, vvll, date, time, system, subsystem, element, element_type, stg, ste, environment = details
+        command = "bright.cmd endevor ret ele {} --typ {} --env {} --sys {} --sub {} --sn {} -i WEBSALC --comment BrightDownload --ccid TEST --tf {} --sm".format(element, element_type, environment,
+        system, subsystem, stg, "{}.{}".format(element, element_type))
+        print "Trying Command {}".format(command)
+        try:
+            out = subprocess.check_output(command.split())
+        except Exception, details:
+            #print get_files
+            print "Error downloading file %" % details
+
+        self.clean_up_mess()
+
     def get_files_by_ccid(self, ccid):
         out = subprocess.check_output(['bright.cmd', 'endevor', 'list', 'elements', '-i', self.instance, '--sm', '--fo', '--env', self.environment, '--sys', self.system, '--sub', self.subsystem, '--rft', 'string'])
         jObj = json.loads(out)
